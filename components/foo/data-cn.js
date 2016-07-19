117ec713d2bf002ce505a87541fd57007b931db6
rahul <ro87630872@126.com>
2016-07-19 09:27:59 +0800

    【UPD】完成地址更新

--- components/foo/data-cn.js
+++ components/foo/data-cn.js
@@ -2,8 +2,8 @@ module.exports = {
     name: '联系方式',
     center:'总部: 中国广东省广州市海珠区广州大道南228号经典居1408室',
     phone:'电话: 02061275402,61275739',
-    fax:'传真: 0086-20-61275817',
+    fax:'传真: 020-61275817',
     address:'厂址: 中国广东省肇庆市大旺高新区迎宾大道创业路文德街3号',
-    email:'邮编:<a href="mailto:sales@renoxbell.com ">sales@renoxbell.com </a>',
+    email:'邮编:<a href="mailto:renox1@renoxbell.com">renox1@renoxbell.com</a>',
     foomsg:'Renoxbell铝业有限公司首页'
 };
--- components/foo/data.js
+++ components/foo/data.js
@@ -2,8 +2,8 @@ module.exports = {
     name: 'Get in touch',
     center:'Headquarter: RM1408, jingdianju Building, No.288 Guangzhou Ave. South, Haizhu District, Guangond Province, Guangzhou City, China',
     phone:'Tel: 0086-20-61275739',
-    fax:'Fax: 0086-20-61275817',
+    fax:'Fax: 020-61275817',
     address:'Factory:  No. Chuangye Road, Wende 3rd Street. Yingbindadao, Dawang Hi-Tech Zone, Zhaoqing City, Guangdong Province, China',
-    email:'E-mail:<a href="mailto:sales@renoxbell.com ">sales@renoxbell.com </a>',
+    email:'E-mail:<a href="mailto:renox1@renoxbell.com">renox1@renoxbell.com</a>',
     foomsg:'Renoxbell Aluminum Co., Ltd. Home'
 };
