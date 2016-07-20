632778825e1b3485b092b325a4ad9b0360bf499d
rahul <ro87630872@126.com>
2016-07-20 08:51:01 +0800

    【UPD】更新

--- components/foo/data-cn.js
+++ components/foo/data-cn.js
@@ -4,6 +4,6 @@ module.exports = {
     phone:'电话: 02061275402,61275739',
     fax:'传真: 020-61275817',
     address:'厂址: 中国广东省肇庆市大旺高新区迎宾大道创业路文德街3号',
-    email:'邮编:<a href="mailto:renox1@renoxbell.com">renox1@renoxbell.com</a>',
+    email:'邮箱:<a href="mailto:renox1@renoxbell.com">renox1@renoxbell.com</a>',
     foomsg:'Renoxbell铝业有限公司首页'
 };
