868984be907510e1670acc039f01b78ac18f3803
rahul <ro87630872@126.com>
2016-08-08 12:04:32 +0800

    【UPD】更新网站文案

--- components/foo/data.js
+++ components/foo/data.js
@@ -4,7 +4,7 @@ module.exports = {
     phone:'Tel:+86-20-61275739, +86-758-3602859',
     fax:'Fax:  +86-758-3602856',
     address:'No.1 Chuangye Road, Wende 3rd Street, Yingbin Dadao, Dawang Hi-Tech Zone, Zhaoqing City, Guangdong Province, China',
-    email:'E-mail:<br/><a href="mailto:sales@renoxbell.com">sales@renoxbell.com</a><br/><a href="mailto:sales1@renoxbell.com">sales1@renoxbell.com</a><br/><a href="mailto:sales2@renoxbell.com">sales2@renoxbell.com</a><a href="mailto:sales3@renoxbell.com">sales3@renoxbell.com</a><br/><a href="mailto:renox1@renoxbell.com">renox1@renoxbell.com</a>',
+    email:'E-mail: <a href="mailto:sales@renoxbell.com">sales@renoxbell.com</a>',
     foomsg:'Renoxbell Aluminum Co., Ltd. Home',
     site:'<a href="http://www.renoxbell.com">www.renoxbell.com</a>'
 };
